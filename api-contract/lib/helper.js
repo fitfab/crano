b6a26a955a69a429c6155c35dd20de5ddbcbe1c2
Miguel Julio <mjulio@artnet.com>
2017-06-27 10:19:44 -0400

    setup contract for the navigation -- this is just a test

---
+++ api-contract/lib/helper.js
@@ -0,0 +1,25 @@
+import Pact from 'pact';
+import path from 'path';
+import request from 'superagent';
+
+const headers = { 'Accept': 'application/json'};
+
+
+export const fetchIt = (url) => {
+    return request.get(url).set(headers)
+}
+
+export const mockProvider = ({port, consumer, provider, PROTOCOL}) => {
+        const _provider = Pact({
+            consumer,
+            provider,
+            port,
+            log: path.resolve(process.cwd(), 'logs', 'mockserver-ui.log'),
+            dir: path.resolve(process.cwd(), 'pacts'),
+            logLevel: 'INFO',
+            ssl: (PROTOCOL === 'https'),
+            spec: 2
+        });
+
+        return _provider;
+}
